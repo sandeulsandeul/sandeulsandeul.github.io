fa3f8e8bb4024303a4a06c2cb0c4c1faebd5e997
sandeulsandeul <wjdtksdk@ewhain.net>
2020-09-08 21:54:15 +0900

    Updates

---
+++ precache-manifest.8ff136de4333fd8c43b03ca42a99205f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc6bc3d17b7cf48140efa12b0117732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57dfcb69e436ebf811e0",
+    "url": "/static/css/main.791fff62.chunk.css"
+  },
+  {
+    "revision": "f195783af0983a2e8053",
+    "url": "/static/js/2.dd764954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd764954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57dfcb69e436ebf811e0",
+    "url": "/static/js/main.2b52bba5.chunk.js"
+  },
+  {
+    "revision": "8ee4fef62f396e2785db",
+    "url": "/static/js/runtime-main.5d4cbdb3.js"
+  },
+  {
+    "revision": "64e11212983a8f63bfe3b8ca972f98d4",
+    "url": "/static/media/star3.64e11212.png"
+  }
+]);
